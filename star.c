6f2eb6fde51451f84a34d86e806bb0039a930122
Mansi Varshney <70686638+mansivar21-22@users.noreply.github.com>
2020-12-22 00:17:32 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    for(i=0;i<=5;i++)
+    {
+      {
+          for (i=0; i<=4; i++)
+          {
+               printf("*");/* code */
+          }
+      }   
+    }
+}
